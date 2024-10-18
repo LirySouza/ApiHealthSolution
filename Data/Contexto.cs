e61c4842519290a7b3eaa8482527ce1d89d5a706
LirySouza <lirianysouza15@gmail.com>
2024-10-18 08:19:02 -0300

    contexto descomentado

--- Data/Contexto.cs
+++ Data/Contexto.cs
@@ -15,7 +15,7 @@ namespace Api.Data
 
         public DbSet<TipoSexoModel> TipoSexo { get; set; }
         public DbSet<PacienteModel> Paciente { get; set; }
-      //  public DbSet<PagamentoModel> Pagamento { get; set; }//
+        public DbSet<PagamentoModel> Pagamento { get; set; }
         public DbSet<ProfissionalModel> Profissional { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -24,7 +24,7 @@ namespace Api.Data
             modelBuilder.ApplyConfiguration(new TipoProfissionalMap());
             modelBuilder.ApplyConfiguration(new TipoSexoMap());
             modelBuilder.ApplyConfiguration(new PacienteMap());
-         //modelBuilder.ApplyConfiguration(new PagamentoMap());//
+            modelBuilder.ApplyConfiguration(new PagamentolMap());
             modelBuilder.ApplyConfiguration(new ProfissionalMap());
             base.OnModelCreating(modelBuilder);
         }
